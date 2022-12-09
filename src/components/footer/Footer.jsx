26d19f1ce03d4d5fb9e254c714b2fb3cbf0abc2d
GZMaster <33158650+GZMaster@users.noreply.github.com>
2022-12-09 11:45:25 +0100

    setting up structure

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.scss";
+const Footer = () => {
+  return (
+    <footer className="Footer">
+      <div className="F_text">
+        <p>Copyright © 2022 Mendx Integrated Limited. All rights reserved.</p>
+        <p>
+          Designed by <span className="F_Team">Retro Developers</span>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
