078819c8b3107b88f6a1b31182d3f873c719d73b
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-01-07 23:17:37 +0100

    set up eslinting for project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,48 @@
+/* eslint-disable no-undef */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module"
-  },
-  plugins: ["prettier", "react"],
-  rules: {
-    "prettier/prettier": "error",
-    "arrow-body-style": "off",
-    "prefer-arrow-callback": "off",
-    indent: ["error", "space"],
-    "linebreak-style": ["error", "windows"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-    "react/react-in-jsx-scope": "off"
-  }
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		"eslint-config-prettier",
+		"eslint:recommended",
+		"plugin:react-hooks/recommended",
+		"plugin:react/recommended",
+		"plugin:prettier/recommended"
+	],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module"
+	},
+	plugins: ["prettier"],
+	rules: {
+		indent: ["error", "tab", { SwitchCase: 1 }],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+		"prefer-arrow-callback": "off",
+		"linebreak-style": ["off", "windows"],
+		"no-unused-vars": "error",
+		"no-console": ["error", { allow: ["warn", "error"] }],
+		"react/react-in-jsx-scope": "off",
+		"react/self-closing-comp": ["error", { component: true, html: true }],
+		"prettier/prettier": [
+			"error",
+			{
+				semi: true,
+				tabWidth: 2,
+				useTabs: true,
+				printWidth: 100,
+				singleQuote: false,
+				trailingComma: "none",
+				endOfLine: "auto",
+				arrowParens: "avoid"
+			}
+		]
+	},
+	settings: {
+		react: {
+			version: "detect"
+		}
+	}
 };
