5554a6653e99f43f9819b9176de9a29f20cba87f
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-01-09 00:49:33 +0100

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,34 @@
 /* eslint-disable no-undef */
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true
-	},
-	extends: [
-		"eslint-config-prettier",
-		"eslint:recommended",
-		"plugin:react-hooks/recommended",
-		"plugin:react/recommended",
-		"plugin:prettier/recommended"
-	],
-	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module"
-	},
-	plugins: ["prettier"],
-	rules: {
-		indent: ["error", "tab"],
-		quotes: ["error", "double"],
-		semi: ["error", "always"],
-		"prefer-arrow-callback": "off",
-		"linebreak-style": ["off", "windows"],
-		"no-unused-vars": "warn",
-		"no-console": ["warn", { allow: ["warn", "error"] }],
-		"react/react-in-jsx-scope": "error",
-		"react/jsx-key": "warn",
-		"react/self-closing-comp": ["warn", { component: true, html: true }],
-		"prettier/prettier": [
-			"error",
-			{
-				semi: true,
-				tabWidth: 1,
-				useTabs: true,
-				printWidth: 100,
-				singleQuote: false,
-				trailingComma: "none",
-				endOfLine: "auto",
-				arrowParens: "avoid",
-				bracketSpacing: true,
-				jsxBracketSameLine: false
-			}
-		]
-	},
-	settings: {
-		react: {
-			version: "detect"
-		}
-	}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint-config-prettier",
+    "eslint:recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prefer-arrow-callback": ["warn", { allowNamedFunctions: true }],
+    "linebreak-style": ["warn", "unix"],
+    "no-unused-vars": "warn",
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "react/react-in-jsx-scope": "error",
+    "react/jsx-key": "warn",
+    "react/self-closing-comp": ["warn", { component: true, html: true }],
+    "prettier/prettier": "warn",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
 };
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -6,93 +6,101 @@ import UseMediaQuery from "../mediaquery/UseMediaQuerry";
 import "./NavBar.scss";
 
 const NavBar = () => {
-	const navigate = useNavigate();
-	const [selectPage, setSelectPage] = useState("Home");
-	let isPageWide = UseMediaQuery("(min-width: 769px)");
+  const navigate = useNavigate();
+  const [selectPage, setSelectPage] = useState("Home");
+  let isPageWide = UseMediaQuery("(min-width: 769px)");
 
-	const handleClick = path => {
-		navigate("/");
-		setSelectPage(path);
-	};
-	const [colorChange, setColorChange] = useState(false);
-	const changeNavbarColor = () => {
-		if (window.scrollY >= 50 || !isPageWide) {
-			setColorChange(true);
-		} else {
-			setColorChange(false);
-		}
-	};
-	window.addEventListener("scroll", changeNavbarColor);
+  const handleClick = (path) => {
+    navigate("/");
+    setSelectPage(path);
+  };
+  const [colorChange, setColorChange] = useState(false);
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 50 || !isPageWide) {
+      setColorChange(true);
+    } else {
+      setColorChange(false);
+    }
+  };
+  window.addEventListener("scroll", changeNavbarColor);
 
-	return (
-		<nav className={colorChange ? "nav_component colorChange" : "nav_component"}>
-			<div className="nav_header">
-				{/* <img className="logo" src={logo} alt="" /> */}
-				<h1 className="nav_title">
-					INTRICATE <span>DS.</span>
-				</h1>
-			</div>
+  return (
+    <nav
+      className={colorChange ? "nav_component colorChange" : "nav_component"}
+    >
+      <div className="nav_header">
+        {/* <img className="logo" src={logo} alt="" /> */}
+        <h1 className="nav_title">
+          INTRICATE <span>DS.</span>
+        </h1>
+      </div>
 
-			{isPageWide ? (
-				<div className="links_content">
-					<div className="center">
-						<Link
-							className={selectPage === "Home" ? "links selected" : "links"}
-							smooth={true}
-							spy={true}
-							to="home"
-							offset={-80}
-							onClick={() => handleClick("Home")}
-						>
-							Home
-						</Link>
-						<Link
-							className={selectPage === "About Us" ? "links selected" : "links"}
-							smooth={true}
-							spy={true}
-							to="about"
-							offset={-80}
-							onClick={() => handleClick("About Us")}
-						>
-							About Us
-						</Link>
-						<Link
-							className={selectPage === "Our Services" ? "links selected" : "links"}
-							smooth={true}
-							spy={true}
-							to="services"
-							offset={-80}
-							onClick={() => handleClick("Our Services")}
-						>
-							Services
-						</Link>
-						<Link
-							className={selectPage === "Projects" ? "links selected" : "links"}
-							smooth={true}
-							spy={true}
-							to="services"
-							offset={-80}
-							onClick={() => handleClick("Projects")}
-						>
-							Projects
-						</Link>
-					</div>
-					<Link
-						className={selectPage === "Contact Us" ? "links Contact_Link selected" : "links Contact_Link"}
-						smooth={true}
-						spy={true}
-						to="contact"
-						offset={-80}
-						onClick={() => handleClick("Contact Us")}
-					>
-						Get in Touch
-					</Link>
-				</div>
-			) : (
-				<BurgerMenu />
-			)}
-		</nav>
-	);
+      {isPageWide ? (
+        <div className="links_content">
+          <div className="center">
+            <Link
+              className={selectPage === "Home" ? "links selected" : "links"}
+              smooth={true}
+              spy={true}
+              to="home"
+              offset={-80}
+              onClick={() => handleClick("Home")}
+            >
+              Home
+            </Link>
+            <Link
+              className={selectPage === "About Us" ? "links selected" : "links"}
+              smooth={true}
+              spy={true}
+              to="about"
+              offset={-80}
+              onClick={() => handleClick("About Us")}
+            >
+              About Us
+            </Link>
+            <Link
+              className={
+                selectPage === "Our Services" ? "links selected" : "links"
+              }
+              smooth={true}
+              spy={true}
+              to="services"
+              offset={-80}
+              onClick={() => handleClick("Our Services")}
+            >
+              Services
+            </Link>
+            <Link
+              className={selectPage === "Projects" ? "links selected" : "links"}
+              smooth={true}
+              spy={true}
+              to="services"
+              offset={-80}
+              onClick={() => handleClick("Projects")}
+            >
+              Projects
+            </Link>
+          </div>
+          <Link
+            className={
+              selectPage === "Contact Us"
+                ? "links Contact_Link selected"
+                : "links Contact_Link"
+            }
+            smooth={true}
+            spy={true}
+            to="contact"
+            offset={-80}
+            onClick={() => handleClick("Contact Us")}
+          >
+            Get in Touch
+          </Link>
+        </div>
+      ) : (
+        <BurgerMenu />
+      )}
+    </nav>
+  );
 };
 
 export default NavBar;
