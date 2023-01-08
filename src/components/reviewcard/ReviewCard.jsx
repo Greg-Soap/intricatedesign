48deafb72099350686e2eb0141d37528c239649a
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2023-01-08 23:40:37 +0100

    navbar and scss reformatting
    completed navbar ,adding of scss breakpoints and other boiler plates

--- src/components/reviewcard/ReviewCard.jsx
+++ src/components/reviewcard/ReviewCard.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import "./ReviewCard.scss";
 
-const ReviewCard = ({ items, classContainer, classWrapper  }) => {
-  const { image, name, review, id } = items;
+// eslint-disable-next-line react/prop-types
+const ReviewCard = ({ items, classContainer, classWrapper }) => {
+	// eslint-disable-next-line react/prop-types
+	const { image, name, review, id } = items;
 
-  return (
-    <article className={classContainer ? classContainer : "Review_Container"}> 
-      <div key={id} className={classWrapper ? classWrapper : "Review_Wrapper"}>
-        <div className="Review_Top">
-          <div className="Review_Image">
-            <img src={image} alt={name} />
-          </div>
-          <h6>{name}</h6>
-        </div>
-        <div className="Review_Bottom">
-          <p>{review}</p>
-        </div>
-      </div>
-    </article>
-  );
+	return (
+		<article className={classContainer ? classContainer : "Review_Container"}>
+			<div key={id} className={classWrapper ? classWrapper : "Review_Wrapper"}>
+				<div className="Review_Top">
+					<div className="Review_Image">
+						<img src={image} alt={name} />
+					</div>
+					<h6>{name}</h6>
+				</div>
+				<div className="Review_Bottom">
+					<p>{review}</p>
+				</div>
+			</div>
+		</article>
+	);
 };
 
 ReviewCard.props = {
-  items: {},
-  classContainer: "Review_Container",
-  classWrapper: "Review_Wrapper",
-}
+	items: {},
+	classContainer: "Review_Container",
+	classWrapper: "Review_Wrapper"
+};
 
 export default ReviewCard;
